3a80357a1d9a1e44fe2c27ccc9bcae667a574330
Marlon van der Linde <marlon250f@gmail.com>
2018-04-19 17:43:20 +0200

    Remove embaressing couts

--- src/weather.cpp
+++ src/weather.cpp
@@ -9,21 +9,16 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	std::cout << argv[2] << " + " << argv[1];
-
 	getLiveData(argv[2], argv[1]);
 	return 0;
 }
 
 void getLiveData(const char* city, const char* apikey)
 {	
-	std::cout << "^^^^^";
 	// hardcoded api url... @todo : fix this bad practice :(
 	const char* apiurl = "api.openweathermap.org/data/2.5/weather?q=";
 
-	std::cout << "****";	
 	fetchboy.setApiKey(apikey);
-	std::cout << "!!!!!";
 	fetchboy.setCity(city);
 	fetchboy.setUrl(apiurl);
 	FetchBoyStruct temp = fetchboy.getCurrent();
