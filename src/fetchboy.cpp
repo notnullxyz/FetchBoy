78f9c01345e366f0fd319dc0a8422779ec370f8f
MarlonV <marlonv@protonmail.com>
2018-04-20 21:06:52 +0200

    Fix the malformed url/unresolved host problem by giving curl the char* it wants instead of std::string

--- src/fetchboy.cpp
+++ src/fetchboy.cpp
@@ -14,13 +14,20 @@ FetchBoyStruct FetchBoy::getCurrent()
     curl = curl_easy_init();
     if (curl)
     {
+        // build a oss of the whole api url, then convert it to a char* for curl.
+        // curlopt url expects a char*... this is not an ideal fix.
         ss << apiPath << queryCity << "&APPID=" << apiKey;
         std::string queryUrl = ss.str();
+        char * apiUrl = new char [queryUrl.length()+1];
+        std::strcpy(apiUrl, queryUrl.c_str());
 
-        std::cout << "QUERY URL '" << queryUrl << "'" << std::endl;
-
-        curl_easy_setopt(curl, CURLOPT_URL, queryUrl);
+        curl_easy_setopt(curl, CURLOPT_URL, apiUrl);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // can follow redirect
+        curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+        curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
+        curl_easy_setopt(curl, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
+        curl_easy_setopt(curl, CURLOPT_DNS_CACHE_TIMEOUT, 2 );
+        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
 
         result = curl_easy_perform(curl); // execute request
 
