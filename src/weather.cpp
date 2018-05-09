09cc820f4f2aae5ef7d427ecf561fbbbb2835d7b
Marlon van der Linde <marlon250f@gmail.com>
2018-05-09 13:58:00 +0200

    Add tabular type formatting for output, as well as a struct to convert weather service data into

--- src/weather.cpp
+++ src/weather.cpp
@@ -26,24 +26,51 @@ void prettyCLIPrint(FetchBoyStruct &response)
 {
 	//std::cout << "DEBUG status: " << response.status << std::endl << "DEBUG message: " << response.message << std::endl;
 	// handle error specifics here... then call out to beautify the Json
-	weatherDataMap m = makeJsonReadable(response.message);
-	std::cout << m << std::endl;
+	//weatherDataMap m = makeJsonReadable(response.message);
+	WeatherStruct weather = convertJsonToStructure(response.message);
+
+	//std::cout << weather.locationName << std::endl;
+	//std::cout << weather.sun.sunrise << std::endl;
+
+	echoRow("Location", 20);
+	echoRow("Sunrise", 20);
+	std::cout << std::endl;
+	echoRow(weather.locationName, 20);
+	echoRow(weather.sun.sunrise, 20);
+	std::cout << std::endl;
+}
+
+/**
+ * Pretty output formatter template for final weather data display
+ */
+template<typename T> void echoRow(T t, const int & width) {
+	std::cout << std::left << std::setw(width) << std::setfill(SEPARATOR) << t;
 }
 
 /**
  * Convert from JSON to whatever is considered 'readable' here.
  * Room for some polymorph perhaps, depending on where this is compiled from/for.
  */
-weatherDataMap makeJsonReadable(std::string &json) {
+WeatherStruct convertJsonToStructure(std::string &json) {
 	Json::Reader jsonReader;
 	Json::Value jsonObject;
-	weatherDataMap retained;
+	WeatherStruct data;
 
 	jsonReader.parse(json, jsonObject);
 
-	std::string Description = "Description-> ";
+	// pack the known response from weather service into my own struct
+	data.locationName = jsonObject["name"].asString();
+	data.conditions = 	jsonObject["weather"][0]["description"].asString();
+	data.baro = 		jsonObject["main"]["pressure"].asInt();
+	data.humidity = 	jsonObject["main"]["humidity"].asInt();
+	data.visibility = 	jsonObject["visibility"].asInt();
+	data.wind.degrees = jsonObject["wind"]["degrees"].asInt();
+	data.wind.speed =	jsonObject["wind"]["speed"].asFloat();
+	data.sun.sunrise = 	jsonObject["sys"]["sunrise"].asString();
+	data.sun.sunset = 	jsonObject["sys"]["sunset"].asString();
+	data.temperature.temperature = 			jsonObject["main"]["temperature"].asFloat();
+	data.temperature.temperatureMaximum = 	jsonObject["main"]["temp_max"].asFloat();
+	data.temperature.temperatureMinimum = 	jsonObject["main"]["temp_min"].asFloat();
 
-	retained.insert(std::make_pair(Description, jsonObject["weather"][0]["description"].asString()));
-
-	return retained;
+	return data;
 }
--- src/weather.h
+++ src/weather.h
@@ -2,16 +2,44 @@
 #define WEATHER_H
 
 #include <iostream>
+#include <iomanip>
 #include "fetchboy.h"
+
 #include <jsoncpp/json/json.h>
-#include <map>
+
+struct Wind {
+    float speed;
+    int16_t degrees;
+};
+
+struct Temperature {
+    float temperature;
+    float temperatureMinimum;
+    float temperatureMaximum;
+};
+
+struct Sun {
+    std::string sunrise;
+    std::string sunset;
+};
+
+struct WeatherStruct {
+    std::string locationName;
+    std::string conditions;
+    int16_t baro;
+    int16_t humidity;
+    int16_t visibility;
+    Wind wind;
+    Temperature temperature;
+    Sun sun;
+};
 
 const std::string APIURL = "api.openweathermap.org/data/2.5/weather?q=";
-
-typedef std::map<std::string, std::string> weatherDataMap;
+const char SEPARATOR = ' ';
 
 void getLiveData(const std::string& city, const std::string& apikey);
 void prettyCLIPrint(FetchBoyStruct &response);
-weatherDataMap makeJsonReadable(std::string &json);
+WeatherStruct convertJsonToStructure(std::string &json);
+template<typename T> void echoRow(T t, const int & width);
 
 #endif
